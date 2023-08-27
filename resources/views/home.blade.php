3e075cf54f1195262d32e68f3d47b7bc86d8b901
Yanuar <yanuarcahyo567@gmail.com>
2023-08-28 02:11:50 +0700

    Inisialisasi Awal

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    <h2>Welcome to GIS IT Telkom Surabaya</h2>
+                    <h5>You are loggin in!</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\HomeController;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+
+Auth::routes();
+Route::post('/login', [LoginController::class, 'authenticate']);
+
+
+Route::middleware(['auth'])->prefix('/Home')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('Home');
+});
+
