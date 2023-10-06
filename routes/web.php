ea5f852252289b4206a308578e234d4451aa7078
Adzanil Rachmadhi P <adzanilrachmadip@gmail.com>
2023-10-06 13:06:00 +0700

    adding interface jumat barokah

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
+    return view('dashboard');
+});
+
+Route::get('/login', function () {
     return view('auth.login');
 });
 
