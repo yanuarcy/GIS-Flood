37b07e6d956de7257150e616a01e597919d37ead
Yanuar <yanuarcahyo567@gmail.com>
2023-10-09 11:27:38 +0700

    Delete Auth & Display Adjustments

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,6 @@
 <?php
 
-use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\HomeController;
-use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,18 +17,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('dashboard');
-});
-
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-
-Auth::routes();
-Route::post('/login', [LoginController::class, 'authenticate']);
-
-
-Route::middleware(['auth'])->prefix('/Home')->group(function () {
-    Route::get('/', [HomeController::class, 'index'])->name('Home');
 });
 
