5785cfd429a4f91da0f15036c7b41f9cf2e2535d
Yanuar <yanuarcahyo@gmail.com>
2023-10-30 10:09:54 +0700

    Change Tables into Datatables

--- resources/views/Tables/FloodArea.blade.php
+++ resources/views/Tables/FloodArea.blade.php
@@ -2,46 +2,62 @@
 
 @section('content')
     <div class="main" id="main">
-        <div class="pagetitle">
-            <h1>Tables</h1>
-            <nav>
-                <ol class="breadcrumb">
-                    {{-- <li class="breadcrumb-item"><a href="index.html">Map</a></li> --}}
-                    <li class="breadcrumb-item active">Flood Area</li>
-                </ol>
-            </nav>
-        </div><!-- End Page Title -->
 
-        <table class="table table-bordered caption-top">
-            <caption>List of Flood Casting</caption>
-            <thead class="table-light text-center">
-                <tr>
-                    <th scope="col">No</th>
-                    <th scope="col">Kota/Kabupaten</th>
-                    <th scope="col">Luas Area Sawah Terdampak</th>
-                    <th scope="col">Estimasi jumlah produksi yang rugi</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Lamongan</td>
-                    <td>25 Ha</td>
-                    <td>0,025 Ton</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jombang</td>
-                    <td>872 Ha</td>
-                    <td>0,872 Ton</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td>Jember</td>
-                    <td>150 Ha</td>
-                    <td>0,15 Ton</td>
-                </tr>
-            </tbody>
-        </table>
+        <!-- Recent Sales -->
+        <div class="col-12">
+            <div class="card recent-sales overflow-auto">
+
+                <div class="card-body">
+                    <div class="card-title">
+                        <h2>Tables</h2>
+                        <nav>
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="#Overview">Tables</a></li>
+                                <li class="breadcrumb-item active">Flood Area</li>
+                            </ol>
+                        </nav>
+                    </div><!-- End Page Title -->
+
+                    <table class="table table-borderless datatable">
+                        <thead>
+                            <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Kota/Kabupaten</th>
+                                <th scope="col">Luas Area Sawah Terdampak</th>
+                                <th scope="col">Estimasi jumlah produksi yang rugi</th>
+                                {{-- <th scope="col">Status</th> --}}
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Lamongan</td>
+                                <td>25 Ha</td>
+                                <td>0,025 Ton</td>
+                                {{-- <td><span class="badge bg-success">Approved</span></td> --}}
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jombang</td>
+                                <td>872 Ha</td>
+                                <td>0,872 Ton</td>
+                                {{-- <td><span class="badge bg-warning">Pending</span></td> --}}
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Jember</td>
+                                <td>150 Ha</td>
+                                <td>0,15 Ton</td>
+                                {{-- <td><span class="badge bg-success">Approved</span></td> --}}
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+
+            </div>
+        </div><!-- End Recent Sales -->
+
+
     </div>
 @endsection
